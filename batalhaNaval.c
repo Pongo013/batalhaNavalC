b242cad358c188a18295248ce499e4962fc34b6f
Davi Rocha <102175365+Pongo013@users.noreply.github.com>
2025-04-04 20:22:33 +0000

    Nivel aventureiro concluido

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,11 +1,45 @@
 #include <stdio.h>
 
+//iniciando as águas e posicionando os navios
+void encherAgua(int tabuleiro[10][10]) {
+    int z;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            //1º navio
+            //verificação começa com a var i para posicionar na horizontal
+            if ((i >= 1 && i <= 4) && j == 0 && tabuleiro[j][i] != 3) {
+                tabuleiro[j][i] = 3;
+                z++;
+            //2º navio
+            //verificação começa com a var j para posicionar na vertical
+            } else if ((j >= 6 && j <= 8) && i == 8 && tabuleiro[j][i] != 3) {
+                tabuleiro[j][i] = 3;
+                z++;
+            //3º navio
+            //primeiro eu defino o alcance do valor das variáveis, e depois verifico se as variáveis (i e j) são de mesmo valor
+            } else if ((i >= 3 && i <= 5) && (j >= 3 && j <= 5) && (i == j) && tabuleiro[j][i] != 3) {
+                tabuleiro[j][i] = 3;
+                z++;
+            //4º navio
+            //para fazer em outra diagonal, precisei verificar se a soma de i e j são nove, pois nessa diagonal, todas as somas serão 9 (0,9 - 1,8 - 2,7)
+            } else if ((i >= 7 && i <= 9) && (j >= 0 && j <= 2) && (i + j == 9) && tabuleiro[j][i] != 3) {
+                tabuleiro[j][i] = 3;
+                z++;
+            } else {
+                tabuleiro[j][i] = 0;
+            }
+        }
+    }
+}
+
 int main() {
+    int tabuleiro[10][10];
+    encherAgua(tabuleiro);
+    //preparando as legendas de fora do tabuleiro
     char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int tabuleiro[10][10];
     
-    //imprimir o tabuleiro
+    //imprimindo o tabuleiro
     for (int j = 0; j <= 9; j++) {
         printf("   %c", coluna[j]);
     }
@@ -13,13 +47,7 @@ int main() {
         printf("\n");
         printf("%i   ", linha[i]);
         for (int k = 0; k <= 9; k++) {
-            if ((k >= 1 && k <= 3) && i == 0) {
-                printf("3   ");
-            } else if ((i >= 6 && i <= 8) && k == 8) {
-                printf("3   ");
-            } else {
-                printf("0   ");
-            }
+            printf("%i   ", tabuleiro[i][k]);
         }
     }
 }
